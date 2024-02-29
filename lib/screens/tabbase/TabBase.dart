370b62128b973353addabff8691814578013d2eb
headzero <youbglue-zero@daum.net>
2024-02-29 17:54:00 +0900

    tab button design change

--- lib/screens/tabbase/TabBase.dart
+++ lib/screens/tabbase/TabBase.dart
@@ -1,3 +1,5 @@
+import 'package:base_flutter/screens/home/HomeScreen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TabBase extends StatefulWidget {
@@ -12,48 +14,91 @@ class TabBase extends StatefulWidget {
 class TabBaseState extends State<TabBase> {
   var _selectedIndex = 0;
 
+  final List<Widget> _widgetOptions = <Widget>[const HomeScreen()];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: _widgetOptions.elementAt(_selectedIndex),
         ),
-        bottomNavigationBar: SizedBox(
-          height: 40,
-          child:
-              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-            OutlinedButton(
-              onPressed: () {
-                onClickTab(1);
-              },
-              child: const Text("1"),
-            ),
-            OutlinedButton(
-              onPressed: () {
-                onClickTab(2);
-              },
-              child: const Text("2"),
-            ),
-            OutlinedButton(
-              onPressed: () {
-                // Navigator.push(context,
-                //     MaterialPageRoute(builder: (context) => const WritePage()));
-              },
-              child: const Text("Write"),
-            ),
-            OutlinedButton(
-              onPressed: () {
-                onClickTab(4);
-              },
-              child: const Text("4"),
-            ),
-            OutlinedButton(
-              onPressed: () {
-                onClickTab(5);
-              },
-              child: const Text("5"),
-            )
-          ]),
+        bottomNavigationBar: Container(
+          width: double.infinity,
+          height: 80,
+          child: Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(16, 4, 16, 8),
+            child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: InkWell(
+                      onTap: () {
+                        onClickTab(0);
+                      },
+                      child: const SizedBox.expand(
+                        child: Icon(
+                          Icons.home_outlined,
+                          size: 28,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: InkWell(
+                      onTap: () {
+                        onClickTab(1);
+                      },
+                      child: const SizedBox.expand(
+                        child: const Icon(
+                          Icons.search_outlined,
+                          size: 28,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: InkWell(
+                      onTap: () {
+                        // Navigator.push(context,
+                        //     MaterialPageRoute(builder: (context) => const WritePage()));
+                      },
+                      child: const SizedBox.expand(
+                        child: const Icon(
+                          Icons.post_add_outlined,
+                          size: 28,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: InkWell(
+                      onTap: () {
+                        onClickTab(2);
+                      },
+                      child: const SizedBox.expand(
+                        child: const Icon(
+                          Icons.account_circle_outlined,
+                          size: 28,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: InkWell(
+                      onTap: () {
+                        onClickTab(3);
+                      },
+                      child: const SizedBox.expand(
+                        child: const Icon(
+                          Icons.settings_outlined,
+                          size: 28,
+                        ),
+                      ),
+                    ),
+                  )
+                ]),
+          ),
         ));
   }
 
