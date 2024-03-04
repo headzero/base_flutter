dbb16b272a839582046077fdf6e51debc7754e3e
headzero <youbglue-zero@daum.net>
2024-03-05 00:45:49 +0900

    HorizontalListView for 'popular' Post items

--- lib/screens/home/HomeScreen.dart
+++ lib/screens/home/HomeScreen.dart
@@ -1,3 +1,5 @@
+import 'package:base_flutter/models/Post.dart';
+import 'package:base_flutter/repositories/HomeRepository.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +13,17 @@ class HomeScreen extends StatefulWidget {
 }
 
 class HomeScreenState extends State<HomeScreen> {
+  HomeRepository repository = HomeRepository();
+  List<Post> popularPosts = List.empty();
+
+  @override
+  void initState() {
+    repository.fetchData().then((resultData) => setState(() {
+          popularPosts = resultData['popular']!;
+        }));
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -170,112 +183,54 @@ class HomeScreenState extends State<HomeScreen> {
   Padding popularCasts() {
     return Padding(
       padding: const EdgeInsetsDirectional.fromSTEB(0, 16, 0, 16),
-      child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsetsDirectional.fromSTEB(0, 8, 0, 0),
-              child: Container(
-                width: 160,
-                height: 200,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Padding(
-                  padding: const EdgeInsetsDirectional.all(8),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(12),
-                        child: const Image(
-                          image: CachedNetworkImageProvider(
-                              'https://images.unsplash.com/photo-1668536784894-f8113d91f991?w=500&h=500'),
-                          width: 140,
-                          height: 140,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      const Text(
-                        'The Daily',
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsetsDirectional.fromSTEB(0, 8, 0, 0),
-              child: Container(
-                width: 160,
-                height: 200,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Padding(
-                  padding: const EdgeInsetsDirectional.all(8),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(12),
-                        child: const Image(
-                          image: CachedNetworkImageProvider(
-                              'https://images.unsplash.com/photo-1613485252551-d95ef3015d0e?w=500&h=500'),
-                          width: 140,
-                          height: 140,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      const Text(
-                        'Serial',
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsetsDirectional.fromSTEB(0, 8, 0, 0),
-              child: Container(
-                width: 160,
-                height: 200,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Padding(
-                  padding: const EdgeInsetsDirectional.all(8),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(12),
-                        child: const Image(
-                          image: CachedNetworkImageProvider(
-                              'https://images.unsplash.com/photo-1557804506-669a67965ba0?w=500&h=500'),
-                          width: 140,
-                          height: 140,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      const Text(
-                        'Radiolab',
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
+      child: Container(
+        width: double.infinity,
+        height: 240,
+        child: ListView.builder(
+            shrinkWrap: true,
+            scrollDirection: Axis.horizontal,
+            itemCount: popularPosts.length,
+            itemBuilder: (context, index) {
+              Post postItem = popularPosts[index];
+              return postCardWidget(postItem);
+            }),
       ),
     );
+  }
+
+  Widget postCardWidget(Post postItem) {
+    return Padding(
+              padding: const EdgeInsetsDirectional.fromSTEB(0, 8, 0, 0),
+              child: Container(
+                width: 160,
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Padding(
+                  padding: const EdgeInsetsDirectional.all(8),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        child: Image(
+                          image: CachedNetworkImageProvider(
+                              postItem.images?.first ?? ""),
+                          width: 140,
+                          height: 140,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Text(
+                        postItem.title,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
   }
 
   Padding categoryFilter() {
