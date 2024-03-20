a20abe16a0c8e7b65f8287d53a3d86f1a9245b19
headzero <youbglue-zero@daum.net>
2024-03-20 22:00:59 +0900

    add dummy PostScreen

---
+++ lib/repositories/PostRepository.dart
@@ -0,0 +1,19 @@
+import 'package:base_flutter/models/Post.dart';
+import 'package:base_flutter/models/User.dart';
+
+class PostRepository {
+  Future<User> fetchUserData(String code) async {
+    return User(
+        name: "Joseph Hale",
+        message: "Craft anything",
+        imageUrl:
+            "https://fastly.picsum.photos/id/491/300/300.jpg?hmac=nqxHz8st4gF72CiLOQovPViZnuvh7nKSmJjMwyfcmW8",
+        code: "test1234");
+  }
+
+  Future<Post> fetchData(String id) async {
+    return Post(title: 'Oprah\'s Book Club', content: "Oprah's Book Club encourages avid readers to discover new authors and books that ignite conversation. Take a look at Oprah's complete list of recommended ...", images: [
+      'https://images.unsplash.com/photo-1421757350652-9f65a35effc7?w=500&h=500'
+    ]);
+  }
+}
---
+++ lib/screens/post/PostScreen.dart
@@ -0,0 +1,91 @@
+import 'package:base_flutter/models/Post.dart';
+import 'package:base_flutter/models/User.dart';
+import 'package:base_flutter/repositories/PostRepository.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class PostScreen extends StatefulWidget {
+  const PostScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return PostScreenState();
+  }
+}
+
+class PostScreenState extends State<PostScreen> {
+  PostRepository repository = PostRepository();
+  late Post post;
+  late User user;
+
+  @override
+  void initState() {
+    repository.fetchUserData("").then((value) => setState(() {
+          user = value;
+        }));
+    repository.fetchData("").then((value) => setState(() {
+          post = value;
+        }));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Post"),
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            ProfileWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget ProfileWidget() {
+    return Container(
+        width: double.maxFinite,
+        alignment: AlignmentDirectional.center,
+        decoration: const BoxDecoration(color: Colors.black12),
+        child: Padding(
+          padding: const EdgeInsetsDirectional.fromSTEB(16, 20, 16, 20),
+          child: Row(mainAxisSize: MainAxisSize.max, children: [
+            Padding(
+              padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 12, 0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Image(
+                  image: CachedNetworkImageProvider(user.imageUrl ?? ""),
+                  width: 60,
+                  height: 60,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 4),
+                    child: Text(
+                      user.name!,
+                      style: const TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.w400),
+                    )),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4),
+                  child: Text(
+                    user.message!,
+                    style: const TextStyle(fontSize: 12),
+                  ),
+                )
+              ],
+            ),
+          ]),
+        ));
+  }
+}
